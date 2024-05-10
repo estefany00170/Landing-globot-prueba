7f7e3c2dd49ae9c12c1fcf6d93130ca0e7a0d36e
estefany00170 <133134954+estefany00170@users.noreply.github.com>
2024-05-10 09:57:31 -0400

    Deploy site

--- _symbols/1b4b5ad4-dc3e-40e4-8fef-65941aa73560.js
+++ _symbols/1b4b5ad4-dc3e-40e4-8fef-65941aa73560.js
@@ -807,15 +807,16 @@ function instance($$self, $$props, $$invalidate) {
 
 	let currentImage = '';
 	let currentAlt = '';
+	let currentDescription = '';
 	currentImage = tarjetas[0].image;
 	currentAlt = 'Default image';
-	tarjetas[0].description;
+	currentDescription = tarjetas[0].description;
 	let previousButton = null;
 
 	function changeimg(i) {
 		$$invalidate(3, currentImage = tarjetas[i].image);
 		$$invalidate(4, currentAlt = tarjetas[i].alt);
-		tarjetas[i].description;
+		currentDescription = tarjetas[i].description;
 		let buttonId = 'box' + (i + 1);
 		let element = document.getElementById(buttonId);
 
@@ -839,9 +840,10 @@ function instance($$self, $$props, $$invalidate) {
 		document.getElementById('imgbox').src = currentImage;
 	}
 
-	window.onload = function () {
-		changeimg(0); // Asume que la tarjeta de atención al cliente es la primera en el array 'tarjetas'
-	};
+	document.addEventListener("DOMContentLoaded", function () {
+		changeimg(1); // Asume que la tarjeta de atención al cliente es la primera en el array 'tarjetas'
+		document.getElementById('imgdesc').innerHTML = currentDescription; // Añade esta línea
+	});
 
 	const click_handler = () => changeimg(0);
 	const click_handler_1 = () => changeimg(1);
