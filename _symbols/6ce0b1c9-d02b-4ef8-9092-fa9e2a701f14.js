4f4e5ef00cfdd3508dd3b3d3d1725645b90ba71d
estefany00170 <133134954+estefany00170@users.noreply.github.com>
2025-01-10 10:40:59 -0300

    Deploy site

--- _symbols/6ce0b1c9-d02b-4ef8-9092-fa9e2a701f14.js
+++ _symbols/6ce0b1c9-d02b-4ef8-9092-fa9e2a701f14.js
@@ -3752,7 +3752,7 @@ function create_fragment(ctx) {
 			t6 = text("Tutoriales");
 			t7 = text(" > ");
 			span0 = element("span");
-			t8 = text("Integración redes sociales");
+			t8 = text("Integración Meta");
 			t9 = text(" > ");
 			span1 = element("span");
 			t10 = text("Whatsapp");
@@ -4157,7 +4157,7 @@ function create_fragment(ctx) {
 			t7 = claim_text(div6_nodes, " > ");
 			span0 = claim_element(div6_nodes, "SPAN", {});
 			var span0_nodes = children(span0);
-			t8 = claim_text(span0_nodes, "Integración redes sociales");
+			t8 = claim_text(span0_nodes, "Integración Meta");
 			span0_nodes.forEach(detach);
 			t9 = claim_text(div6_nodes, " > ");
 			span1 = claim_element(div6_nodes, "SPAN", { style: true });
